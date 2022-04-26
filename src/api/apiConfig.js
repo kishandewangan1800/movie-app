86206d2b06733d8720193c8ea839a04f190eed07
Kishan Dewangan <kishan.dewangan1800@gmail.com>
2022-04-26 11:20:11 +0530

    Project

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl : 'https://api.themoviedb.org/3/',
-    apiKey : 'ec7f8b6c6b4494842644d08eeeb1d57c',
+    apiKey : 'tmbd api',
     originalImage: (imgPath)=> `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image : (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
